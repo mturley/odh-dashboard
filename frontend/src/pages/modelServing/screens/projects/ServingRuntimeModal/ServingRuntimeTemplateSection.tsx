c1d57bf5510086aa53ae6786f33524a07de15bad
Juntao Wang <juntwang@redhat.com>
2024-11-01 11:02:09 -0400

    Follow-up fixes in Model Deployment modals for Select component

--- frontend/src/pages/modelServing/screens/projects/ServingRuntimeModal/ServingRuntimeTemplateSection.tsx
+++ frontend/src/pages/modelServing/screens/projects/ServingRuntimeModal/ServingRuntimeTemplateSection.tsx
@@ -18,6 +18,7 @@ type ServingRuntimeTemplateSectionProps = {
   templates: TemplateKind[];
   isEditing?: boolean;
   selectedAcceleratorProfile: AcceleratorProfileFormData;
+  resetModelFormat?: () => void;
 };
 
 const ServingRuntimeTemplateSection: React.FC<ServingRuntimeTemplateSectionProps> = ({
@@ -26,6 +27,7 @@ const ServingRuntimeTemplateSection: React.FC<ServingRuntimeTemplateSectionProps
   templates,
   isEditing,
   selectedAcceleratorProfile,
+  resetModelFormat,
 }) => {
   const filteredTemplates = React.useMemo(
     () =>
@@ -74,7 +76,15 @@ const ServingRuntimeTemplateSection: React.FC<ServingRuntimeTemplateSectionProps
         toggleProps={{ id: 'serving-runtime-template-selection' }}
         value={data.servingRuntimeTemplateName}
         onChange={(name) => {
-          setData('servingRuntimeTemplateName', name);
+          // Avoid onChange function is called twice
+          // In KServe modal, it would set the model framework field to empty if there is only one option
+          if (name !== data.servingRuntimeTemplateName) {
+            setData('servingRuntimeTemplateName', name);
+            // Reset model framework selection when changing the template in KServe modal only
+            if (resetModelFormat) {
+              resetModelFormat();
+            }
+          }
         }}
       />
     </FormGroup>
